7e2f9f2d98de96203349a60e12f29db59f867761
Raasmus nilsson <raasmusnilsson@gmail.com>
2021-05-11 23:13:33 +0200

    Trying to add dist to Github Pages

---
+++ precache-manifest.67d68b5420802a1942fa27824a9dcea7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b93a8da128228196804",
+    "url": "/css/app.f3d9da91.css"
+  },
+  {
+    "revision": "a12e99cf190c1ced612e088e7d9defc7",
+    "url": "/img/meme-logo.a12e99cf.png"
+  },
+  {
+    "revision": "80775bca54b76162ed0cba9042839045",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b93a8da128228196804",
+    "url": "/js/app.3670e891.js"
+  },
+  {
+    "revision": "67cdcce3052e03a0dc0b",
+    "url": "/js/chunk-vendors.a83e292f.js"
+  },
+  {
+    "revision": "7756abe3c2b27ebffe1353ae579857a9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "53c2b5e074547b9007a7e2eded4f06f6",
+    "url": "/manifest.webmanifest"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,37 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.67d68b5420802a1942fa27824a9dcea7.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "my-project"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute("https://api.imgflip.com/get_memes", new workbox.strategies.NetworkFirst({ "networkTimeoutSeconds":5, plugins: [] }), 'GET');
+workbox.routing.registerRoute("https://api.chucknorris.io/jokes/random", new workbox.strategies.NetworkFirst({ "networkTimeoutSeconds":5, plugins: [] }), 'GET');
